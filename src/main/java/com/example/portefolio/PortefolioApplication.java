8fd4e71633766bc895a493c14337c07e2d4cb9b6
johanne <johanne_riis@hotmail.com>
2022-03-01 10:02:06 +0100

    HomePage Fits a 13" screen, semi-responsive. Layout on other screens isn't great yet.

---
+++ src/main/java/com/example/portefolio/PortefolioApplication.java
@@ -0,0 +1,13 @@
+package com.example.portefolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortefolioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortefolioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/portefolio/PortefolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.portefolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortefolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
